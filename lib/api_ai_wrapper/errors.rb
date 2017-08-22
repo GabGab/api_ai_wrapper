2577895260ef8898282f4796568585aceb9b9b7d
Vincent Gabou <vincent.gabou@gmail.com>
2017-08-22 17:25:11 +0200

    [1.0.5] Fixed custom error system with correct message, status and code fields

--- lib/api_ai_wrapper/errors.rb
+++ lib/api_ai_wrapper/errors.rb
@@ -1,28 +1,34 @@
 class ApiAiWrapper::Errors
 
-  module Request
-    class UnsupportedParams < StandardError; end
-  end
-
   module Engine
-    class MissingTokens < StandardError
-      def initialize(_message = "You have not set a developer or client token for this engine")
-        @message = _message
+    class Error < StandardError
+      def to_s
+        @message
       end
     end
 
-    class MissingToken < StandardError
-      def initialize(_message)
-        @message = "Unauthorized call - #{_message}"
+    class MissingTokens < Error
+      def initialize(message = nil)
+        @message = message || "You have not set a developer or client token for this engine."
+      end
+    end
+
+    class MissingToken < Error
+      attr_reader :code
+
+      def initialize(message = nil)
+        @message = message || "Unauthorized call."
         @code = 401
       end
     end
 
-    class ApiError < StandardError
-      def initialize(_message, _code, _status)
-        @message = _message
-        @code = _code
-        @status = _status
+    class ApiError < Error
+      attr_reader :code, :status
+
+      def initialize(message = nil, code = nil, status = nil)
+        @message = message || "An error occured in API.AI."
+        @code = code
+        @status = status
       end
     end
 
